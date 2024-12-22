56c3fdebc5ebc39fd9dd35465ebc5820a8f281f8
Ahmet Oğuz Ergin <ahmettoguz@hotmail.com>
2024-12-22 16:50:04 +0300

    remove basic pg connection as integration of prisma

--- src/env/envVariables.ts
+++ src/env/envVariables.ts
@@ -3,11 +3,6 @@ dotenv.config();
 
 const envVariables = {
   PORT: process.env.PORT || 80,
-
-  DBHOST: process.env.DBHOST,
-  DBDATABASE: process.env.DBDATABASE,
-  DBPGPASSWORD: process.env.DBPGPASSWORD,
-  DBUSERNAME: process.env.DBUSERNAME,
 };
 
 export default envVariables;
