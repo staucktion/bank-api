f660f26b0603740325a994e5a50b17e339ef6551
Ahmet Oğuz Ergin <ahmettoguz@hotmail.com>
2025-01-04 15:03:19 +0300

    update validation util

--- src/util/ValidationUtil.ts
+++ src/util/ValidationUtil.ts
@@ -7,6 +7,12 @@ class ValidationUtil {
 		if (obj === "") CustomError.builder().setErrorType("Object Validation").setExternalMessage(`Object is empty string.`).build().throwError();
 	}
 
+	public static checkArrayData(arr) {
+		if (typeof arr !== "object") CustomError.builder().setErrorType("Array Validation").setExternalMessage(`Parameter is not an object.`).build().throwError();
+		if (!Array.isArray(arr)) CustomError.builder().setErrorType("Array Validation").setExternalMessage(`Object is not an array.`).build().throwError();
+		if (arr.length === 0) CustomError.builder().setErrorType("Data Validation").setExternalMessage(`Array is empty.`).build().throwError();
+	}
+
 	public static checkRequiredFields(requiredFields: string[], obj) {
 		const missingFields = requiredFields.filter((field) => obj[field] === undefined || obj[field] === null || obj[field] === "");
 
